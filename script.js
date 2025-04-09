59b3c4271d989bb6cc1058cddc0db6bbcb5c9a4a
Gabriel William <100963685+gabrielxx7@users.noreply.github.com>
2025-04-09 17:37:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem() {
+    
+    var userInput = document.getElementById("userInput")?.value;
+
+   
+    console.log("Parabéns, você clicou no botão :)");
+
+    document.getElementById("mensagem").innerText = "Parabéns, você clicou no botão :)";
+}
